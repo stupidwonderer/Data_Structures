11653c875334154b235494a412b0f3a8539e6e0a
stupidwonderer <83151106+stupidwonderer@users.noreply.github.com>
2022-07-14 21:23:40 +0530

    Add files via upload

---
+++ StackClass.java
@@ -0,0 +1,57 @@
+public class StackClass {
+    static class Node{
+        int data;
+        Node next;
+    Node(int data){
+        this.data=data;
+        next=null;
+    }
+    }
+    static class Stack{
+        public static Node head=null;
+        public static boolean isEmpty(){
+            return (head==null);
+        }
+        
+        public void push(int data){
+            Node newNode = new Node(data);
+            if (head==null) {
+                head=newNode;
+                return;
+            }
+            newNode.next=head;
+            head=newNode;
+
+
+        }
+
+        public static int pop(){
+            if (isEmpty()==true) return -1;
+            Node top=head;
+            head=head.next;
+            return (top.data);
+        }
+
+        public static int peek(){
+            if (isEmpty()==true) return -1;
+            Node top= head;
+            return ((top.data));
+        }
+     }
+     public static void main(String[] args) {
+        Stack stc = new Stack();
+        System.out.println(stc.isEmpty());
+        stc.push(1);
+        stc.push(2);
+        stc.push(3);
+        stc.push(4);
+        stc.push(5);
+        while(!stc.isEmpty()) {
+            System.out.println(stc.peek());
+            stc.pop();
+        }
+        System.out.println(stc.isEmpty());
+
+     }
+    
+}
